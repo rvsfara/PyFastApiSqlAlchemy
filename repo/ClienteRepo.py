16092bbcc08523710461c821bd1e3764f8610fc7
Rafael Vieira Dos Santos <rvsfara@gmail.com>
2022-10-09 20:35:07 -0300

    comentarios para facilitar leitura

--- repo/ClienteRepo.py
+++ repo/ClienteRepo.py
@@ -4,10 +4,12 @@ from models import Cliente
 
 
 class ClienteRepository:
+    # busca todos os Clientes Cadastrados
     @staticmethod
     def find_all(db: Session) -> list[Cliente]:
         return db.query(Cliente).all()
-
+    
+    # salva um novo Cliente ou edita um já existente com com o merge
     @staticmethod
     def save(db: Session, cliente: Cliente) -> Cliente:
         if cliente.id:
@@ -16,15 +18,15 @@ class ClienteRepository:
             db.add(cliente)
         db.commit()
         return cliente
-
+    # busca um Cliente com base no id
     @staticmethod
     def find_by_id(db: Session, id: int) -> Cliente:
         return db.query(Cliente).filter(Cliente.id == id).first()
-
+    # verificar se existe algum Cliente cadastrado com base no ID
     @staticmethod
     def exists_by_id(db: Session, id: int) -> bool:
         return db.query(Cliente).filter(Cliente.id == id).first() is not None
-
+    # excluir um Cliente com base no id
     @staticmethod
     def delete_by_id(db: Session, id: int) -> None:
         cliente = db.query(Cliente).filter(Cliente.id == id).first()
--- repo/CursoRepo.py
+++ repo/CursoRepo.py
@@ -4,10 +4,12 @@ from models import Curso
 
 
 class CursoRepository:
+    # busca todos os Cursos Cadastrados
     @staticmethod
     def find_all(db: Session) -> list[Curso]:
         return db.query(Curso).all()
-
+    
+    # salva um novo curso ou edita um já existente com com o merge
     @staticmethod
     def save(db: Session, curso: Curso) -> Curso:
         if curso.id:
@@ -16,15 +18,16 @@ class CursoRepository:
             db.add(curso)
         db.commit()
         return curso
-
+    
+    # busca um Curso com base no id
     @staticmethod
     def find_by_id(db: Session, id: int) -> Curso:
         return db.query(Curso).filter(Curso.id == id).first()
-
+    # verificar se existe algum curso cadastrado com base no ID
     @staticmethod
     def exists_by_id(db: Session, id: int) -> bool:
         return db.query(Curso).filter(Curso.id == id).first() is not None
-
+    # excluir um curso com base no id
     @staticmethod
     def delete_by_id(db: Session, id: int) -> None:
         curso = db.query(Curso).filter(Curso.id == id).first()
